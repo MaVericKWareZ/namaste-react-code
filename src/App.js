0eed27821c2bed26ea46a3cda977f7d33149f0a6
Sarthak Mahapatra <sarthakchampion07@gmail.com>
2023-12-28 11:17:29 +0530

    code from chapter 1

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("h1", { id: "heading1" }, "Hello World from h1!"),
+  React.createElement("h2", { id: "heading2" }, "Hello World from h2!"),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
