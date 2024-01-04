3af6ea08570ebe87fdecc2275cfa59c6f6a6f5f9
Sarthak Mahapatra <sarthakchampion07@gmail.com>
2024-01-04 14:33:41 +0530

    ch2 commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", { id: "heading1" }, "Hello World from h1!"),
   React.createElement("h2", { id: "heading2" }, "Hello World from h2!"),
