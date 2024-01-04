900857927ecae5b6eb168856c83a1f0028c2187d
Sarthak Mahapatra <sarthakchampion07@gmail.com>
2024-01-04 21:19:06 +0530

    ch3 commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "heading1" }, "Hello World from h1!"),
-  React.createElement("h2", { id: "heading2" }, "Hello World from h2!"),
-]);
-console.log(parent);
+//  React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Welcome to Namaste React 🚀
+  </h1>
+);
+
+// React Component
+
+const HeadingComponent = () => {
+  return <h1>Namaste React from functional component</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
