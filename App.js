bc79e5cb567072cd1ce475172a8ed7484d9c869b
Sarthak Mahapatra <sarthakchampion07@gmail.com>
2024-01-07 14:46:10 +0530

    ch4 commit 1

--- App.js
+++ App.js
@@ -1,23 +1,101 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>Hello! </span>;
-const title = (
-  <h1 className="head" tabIndex="5">
-    {elem}
-    Namaste React using JSX 🚀
-  </h1>
-);
+import logo from "./logo.png";
+/*
 
-const HeadingComponent = () => (
-  <div id="container">
-    {title}
-    <h1 className="heading">
-      Namaste React from functional component with no return 🚀
-    </h1>
-  </div>
-);
+- Head 
+    - Logo
+    - Nav Item
+- Body
+    - Search 
+    - Restaurant Container
+        - Restaurant Card
+            - Image
+            - Name
+            - Cuisine
+            - Rating
+            - ETA
+- Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://image.similarpng.com/very-thumbnail/2021/07/Chef-restaurant-logo-illustrations-template-on-transparent-background-PNG.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <div className="res-image-container">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/k0ajwyrofyptgnvjotr5"></img>
+      </div>
+      <div className="res-detail-container">
+        <h3>Meghana Foods</h3>
+        <h4>Biryani, Indian </h4>
+        <h4>4.4 Stars </h4>
+        <h4>2000 for Two </h4>
+        <h4>30 mins ETA </h4>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input type="text" placeholder="Enter search keyword" />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<App />);
